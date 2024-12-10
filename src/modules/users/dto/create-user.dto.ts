6dcc639c9280cb70f01f1cbd043afda27550821b
petsereypanha <sereypanhapet@gmail.com>
2024-12-10 23:22:42 +0700

    update i18n

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -8,14 +8,20 @@ import {
   MinLength,
   Matches,
 } from 'class-validator';
-import { Exclude, Expose, Transform } from 'class-transformer';
+import { Transform } from 'class-transformer';
+import { i18nValidationMessage } from 'nestjs-i18n';
+import type { I18nTranslations } from 'src/modules/common/generated/i18n.generated';
 
 export class CreateUserDto {
   @ApiProperty({ description: 'Name of the user', minLength: 3 })
-  @MinLength(3, { message: 'Name Min 3 Character' })
+  @MinLength(3, {
+    message: i18nValidationMessage<I18nTranslations>('validation.minLength', {
+      min: 3,
+    }),
+  })
   @IsString()
-  @Matches(/^[a-zA-Z0-9\s]*$/, {
-    message: 'Name can only contain alphanumeric characters',
+  @Matches(/^[a-zA-Z\s]*$/, {
+    message: i18nValidationMessage<I18nTranslations>('validation.alphanumeric'),
   })
   name: string;
 
@@ -23,33 +29,41 @@ export class CreateUserDto {
     description: 'Email of the user',
     example: 'user@example.com',
   })
-  @IsEmail({}, { message: 'Invalid Email' })
+  @IsEmail(
+    {},
+    { message: i18nValidationMessage<I18nTranslations>('validation.invalid') },
+  )
   @Matches(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, {
-    message: 'Invalid email format',
+    message: i18nValidationMessage<I18nTranslations>('validation.format'),
   })
   email: string;
 
   @ApiProperty({ description: 'Password of the user', minLength: 8 })
-  @MinLength(8, { message: 'Password Min 8 Character' })
+  @MinLength(8, {
+    message: i18nValidationMessage<I18nTranslations>('validation.minLength', {
+      min: 8,
+    }),
+  })
   @Matches(/^(?=.*[A-Z])/, {
-    message: 'Password must contain at least one uppercase letter',
+    message: i18nValidationMessage<I18nTranslations>('validation.uppercase'),
   })
   @Matches(/^(?=.*[a-z])/, {
-    message: 'Password must contain at least one lowercase letter',
+    message: i18nValidationMessage<I18nTranslations>('validation.lowercase'),
   })
   @Matches(/^(?=.*[0-9])/, {
-    message: 'Password must contain at least one number',
+    message: i18nValidationMessage<I18nTranslations>('validation.number'),
   })
   @Matches(/^(?=.*[!@#$%^&*])/, {
-    message: 'Password must contain at least one special character (!@#$%^&*)',
-  })
-  @MinLength(8, {
-    message: 'Password must be at least 8 characters long',
+    message: i18nValidationMessage<I18nTranslations>(
+      'validation.special_character',
+    ),
   })
   password: string;
 
   @ApiProperty({ description: 'Role of the user', enum: UserRole })
-  @IsEnum(UserRole, { message: 'Select Role' })
+  @IsEnum(UserRole, {
+    message: i18nValidationMessage<I18nTranslations>('validation.role'),
+  })
   role: UserRole;
 
   @ApiProperty({
@@ -57,7 +71,9 @@ export class CreateUserDto {
     enum: UserStatus,
     required: false,
   })
-  @IsEnum(UserStatus, { message: 'Select Status' })
+  @IsEnum(UserStatus, {
+    message: i18nValidationMessage<I18nTranslations>('validation.status'),
+  })
   @IsOptional()
   @Transform(({ value }) => value?.toUpperCase())
   status: UserStatus;
